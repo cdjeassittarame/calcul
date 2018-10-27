d1425375943a66c24f11424f92e91dc00b19a5e4
chris djeassi <c.djeassittarame@live.fr>
2018-10-27 23:50:54 +0200

    clean code

--- src/main/com/company/Main.java
+++ src/main/com/company/Main.java
@@ -1,21 +1,28 @@
 package main.com.company;
 
+
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int nombreCalcul=0;
         String nombre1=null;
         String nombre2=null;
         String Calcul=null;
+        int integerCalcul=0;
+        String Recommencer=null;
+
         System.out.println("Bienvenue dans votre calculatrice");
         do {
+
             System.out.println("Veuillez Choisir un type de calcul");
-            Scanner scannerCalcul = new Scanner(System.in);
-            System.out.println("1.Addition 2.Soustraction 3.Multiplication 4.Division 5.Reset");
-            Calcul = scannerCalcul.nextLine();
-            int integerCalcul = Integer.parseInt(Calcul);
+            do {
+                Scanner scannerCalcul = new Scanner(System.in);
+                System.out.println("1.Addition 2.Soustraction 3.Multiplication 4.Division 5.Reset");
+                Calcul = scannerCalcul.nextLine();
+                integerCalcul = Integer.parseInt(Calcul);
+            }while(integerCalcul < 1 | integerCalcul > 5);
 
             if (integerCalcul == 5){
                 nombreCalcul = 0;
@@ -29,7 +36,11 @@ public class Main {
 
 
             Scanner scannerNombre2 = new Scanner(System.in);
-            System.out.println("Veuillez Choisir un deuxieme nombre");
+            if(nombreCalcul != 0){
+                System.out.println("Veuillez Choisir un deuxieme nombre à calculer avec " + nombreCalcul);
+            }else {
+                System.out.println("Veuillez Choisir un deuxieme nombre");
+            }
             nombre2 = scannerNombre2.nextLine();
 
             try {
@@ -40,34 +51,52 @@ public class Main {
                 if (integerCalcul >= 1 & integerCalcul <= 4) {
                     switch (integerCalcul) {
                         case 1:
+                            System.out.println(nombreCalcul + " + " + integerNombre2);
                             nombreCalcul = nombreCalcul + integerNombre2;
                             nombre1 = String.valueOf(nombreCalcul);
-                            System.out.println(nombreCalcul);
+                            System.out.println("Resultat: " + nombreCalcul);
                             break;
                         case 2:
+                            System.out.println(nombreCalcul + " - " + integerNombre2);
                             nombreCalcul = nombreCalcul - integerNombre2;
-                            nombre1 = String.valueOf(nombreCalcul);
-                            System.out.println(nombreCalcul);
+
+                            if(nombreCalcul < 0){
+                                System.out.println("Resultat: " + Math.abs(nombreCalcul) * (-1));
+                                nombre1 = String.valueOf(nombreCalcul);
+                            }else {
+                                nombre1 = String.valueOf(nombreCalcul);
+                                System.out.println("Resultat: " + nombreCalcul);
+                            }
                             break;
                         case 3:
+                            System.out.println(nombreCalcul + " * " + integerNombre2);
                             nombreCalcul = nombreCalcul * integerNombre2;
                             nombre1 = String.valueOf(nombreCalcul);
-                            System.out.println(nombreCalcul);
+                            System.out.println("Resultat: " + nombreCalcul);
                             break;
                         case 4:
+                            System.out.println(nombreCalcul + " / " + integerNombre2);
                             nombreCalcul = nombreCalcul / integerNombre2;
                             nombre1 = String.valueOf(nombreCalcul);
-                            System.out.println(nombreCalcul);
+                            System.out.println("Resultat: " + nombreCalcul);
                             break;
                         default:
                             System.out.println("null");
                     }
                 }
+
+
+                    Scanner scannerRecommencer = new Scanner(System.in);
+                    System.out.println("Voulez-vous recommencer Y ou N");
+                    Recommencer = scannerRecommencer.nextLine();
+
+
             } catch (NumberFormatException e) {
                 System.out.println("Error: " + e);
                 return;
             }
-        }while(true);
+        }while(Recommencer.equals("Y"));
 
     }
+
 }
